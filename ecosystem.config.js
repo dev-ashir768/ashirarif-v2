bf0b31cecb3e70d56aa5a438bbcd8d4f770aca85
dev-ashir768 <techashir167@gmail.com>
2026-01-02 22:31:08 +0500

    add server.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,8 @@ module.exports = {
   apps: [
     {
       name: "ashirarif",
-      script: "./server.js",
+      script: "bun",
+      args: "run server.ts",
       cwd: "/public_html/repo",
       instances: 1,
       exec_mode: "fork",
@@ -11,12 +12,10 @@ module.exports = {
       max_memory_restart: "1G",
       min_uptime: "10s",
       max_restarts: 10,
-      node_args: "--max-old-space-size=1024",
       cron_restart: "0 4 * * *",
       env: {
         NODE_ENV: "production",
         PORT: 3000,
-        NODE_OPTIONS: "--max-old-space-size=1024",
       },
       error_file: "/public_html/repo/logs/err.log",
       out_file: "/public_html/repo/logs/out.log",

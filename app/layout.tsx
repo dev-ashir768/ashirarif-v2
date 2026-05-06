65d0de3ac46bf59b459eb7cfaa87647b124230e7
Ashir Arif <techashir167@gmail.com>
2026-05-06 11:21:55 +0500

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,12 +34,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <Script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7464685771078160"
-        crossOrigin="anonymous"
-        strategy="beforeInteractive"
-      />
       <body
         className={cn(
           outfit.variable,
